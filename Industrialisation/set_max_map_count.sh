5d59d2d41ab93f4be048721bcdee688cd4abaf38
Vincent Le Roux <vincent.leroux84@gmail.com>
2025-09-12 08:59:55 +0200

    v2

--- Industrialisation/set_max_map_count.sh
+++
@@ -1,8 +0,0 @@
-# Augmentation du parametre vm_max_map_count et vérification que tout est OK
-echo "Augmentation du parametre vm_max_map_count"
-sysctl -w vm.max_map_count=262144
-
-
-# Redémarrage de Docker
-echo "Redemarrage de Docker"
-systemctl restart docker
